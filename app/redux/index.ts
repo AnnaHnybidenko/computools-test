d44d761e7176f6714e51d5b868f9f4a90fdf62d3
Anna Hnybidenko <hnybidenkowork@gmail.com>
2021-10-02 13:55:14 +0300

    fix(redux): combineReducers

--- app/redux/index.ts
+++ app/redux/index.ts
@@ -1,7 +1,11 @@
-import {configureStore} from '@reduxjs/toolkit';
+import {configureStore, combineReducers} from '@reduxjs/toolkit';
 
 import {userSlice} from './user';
 
+const rootReducer = combineReducers({
+  user: userSlice.reducer,
+})
+
 export const store = configureStore({
-  reducer: userSlice.reducer,
+  reducer: rootReducer,
 });
